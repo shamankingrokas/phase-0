5dcfa1cfee44a3e24c954fefa4d4818bc060d599
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-01-26 21:26:37 -0600

    Framework for pairing challenge

---
+++ week-5/calculate-mode/my_solution.rb
@@ -0,0 +1,45 @@
+# Calculate the mode Pairing Challenge
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented.
+
+
+
+# 0. Pseudocode
+
+# What is the input?
+# What is the output? (i.e. What should the code return?)
+# What are the steps needed to solve the problem?
+=begin
+
+ 1. Go through array compare and compare each index
+ 2.
+
+=end
+
+# 1. Initial Solution
+def mode(array)
+  array.each do |n|
+
+  end
+
+
+end
+
+array = ["A", "B", "C"]
+array.each_with_index {|val, index| puts "#{val} => #{index}" }
+
+
+
+
+# 3. Refactored Solution
+
+
+
+
+# 4. Reflection
