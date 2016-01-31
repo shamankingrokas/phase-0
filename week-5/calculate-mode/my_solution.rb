0118d9bdef2d4a7c600d831f9f4c1ddc58265855
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-01-31 11:26:36 -0600

    5.3 Calculating the mode

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -13,28 +13,48 @@
 # 0. Pseudocode
 
 # What is the input?
+# The array
+
 # What is the output? (i.e. What should the code return?)
+# Single element array with the mode(s)
+
 # What are the steps needed to solve the problem?
-=begin
+# 1. Verify if the array is empty is_empty?
+# 2. Create an empty hash
+# 3. Define max as 0
+# 4. Iterate through array and add each item into a indexed hash with a key and value
+#
 
- 1. Go through array compare and compare each index
- 2.
 
-=end
 
 # 1. Initial Solution
-def mode(array)
-  array.each do |n|
+def mode (array)
+  if array.empty?
+    return array
+  else
+    hash = {}
+    max = 0
+    array.each do |index|
+      quant = array.count(index)
+      hash[index] = quant
+
+    if quant > max
+      max = quant
+    end
+  end
+  hash_mode = hash.select {|key, value| value == max}
+  p hash_mode.keys
 
   end
 
-
 end
 
-array = ["A", "B", "C"]
-array.each_with_index {|val, index| puts "#{val} => #{index}" }
-
-
+mode([1,2,3,3])         # => [3]
+mode([4.5, 0, 0])       # => [0]
+mode([1.5, -1, 1, 1.5]) # => [1.5]
+mode([1,1,2,2])         # => [1,2]
+mode([1,2,3])           # => [1,2,3], because all occur with equal frequency
+mode(["who", "what", "where", "who"]) # => ["who"]
 
 
 # 3. Refactored Solution
